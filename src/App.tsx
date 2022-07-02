801440d9406e9fd8168d3fa3dcd5b8f51523a673
Paweł Ciszewski <kardahim@gmail.com>
2022-07-02 22:17:15 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// components
+import Advice from './components/advice/index'
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
